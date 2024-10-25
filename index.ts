7b33e8567966fb8c492e4304664c7a37d5f21038
Mikael Amare <mikael.amare@mths.ca>
2024-10-25 18:55:55 -0400

    stuff

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 /*
- * /*
  * Given a string, returns the length of
  *  the largest run in the string.
  * A "run" is a series of zero or
@@ -9,20 +8,19 @@
  * @since   2024-10-18
  */
 
-function maxRun(str: string): number {
-    if (str.length === 0) return 0;
+public int maxRun(String str) {
+    if (str.length() == 0) return 0;
 
-    let maxRun = 1;
-    let currentRun = 1;
+    int maxRun = 1; // At least one character in a run
+    int currentRun = 1;
 
-    for (let counter = 1; counter < str.length; counter++) {
-        if (str[counter] === str[counter - 1]) {
+    for (int counter = 1; counter < str.length(); counter++) {
+        if (str.charAt(counter) == str.charAt(counter - 1)) {
             currentRun++;
         } else {
             maxRun = Math.max(maxRun, currentRun);
-            currentRun = 1;
+            currentRun = 1; // Reset the current run count for the new character
         }
     }
-
     return Math.max(maxRun, currentRun); // Final comparison to account for end of string
 }
